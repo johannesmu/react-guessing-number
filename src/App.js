dd07c18582150bda627c889232ac0ac3f0e61caa
johannes <jmuljana@gmail.com>
2021-03-06 11:06:01 +1100

    fixed state output in message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import {useState,useEffect} from 'react'
 import './App.css';
 
+function generateRandom() {
+  return Math.round(Math.random() * 100)
+}
+
 function App() {
+  const [message,setMessage] = useState('Have a guess')
+  const [secret,setSecret] = useState( generateRandom() )
+  const SubmitHandler = (event) => {
+    event.preventDefault()
+    const data = new FormData(event.target)
+    const userGuess = parseInt( data.get('guess'))
+    // verify if user guess matches secret
+    if( userGuess === secret ) {
+      setMessage('You guessed correct! The number is '+ {secret})
+    }
+    else if(userGuess > secret ) {
+      setMessage('The number is smaller than '+ userGuess )
+    }
+    else if(userGuess < secret){
+      setMessage('The number is larger than '+ userGuess)
+    }
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Guess My Number</h1>
       </header>
+      <form id="form" onSubmit={SubmitHandler}>
+        <input type="text" name="guess" autoComplete="password" autoFocus="true"/>
+        <button type="submit">Submit</button>
+      </form>
+      <p className="message">{message}</p>
     </div>
   );
 }
